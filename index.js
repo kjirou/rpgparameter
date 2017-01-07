5e3a35a0ee0c3aab77545f194c8e186b31502039
kjirou <kjirou.web@gmail.com>
2017-01-07 20:05:59 +0900

    Add some apis

--- index.js
+++ index.js
@@ -6,6 +6,153 @@ var aggregators = require('./lib/aggregators');
 var utils = require('./lib/utils');
 
 
+var PARAMETER_TYPES = {
+  BOOLEAN: 'BOOLEAN',
+  CHANCE: 'CHANCE',
+  INTEGER: 'INTEGER',
+  NUMBER: 'NUMBER',
+  PARAMETER: 'PARAMETER',
+  RATE: 'RATE'
+};
+
+/**
+ * @return {{type, getDisplayName, validateValue}}
+ */
+var createParameterShape = function createParameterShape(options) {
+  var defaultedOptions = assign({
+    type: PARAMETER_TYPES.PARAMETER,
+
+    defaultValue: null,
+
+    validate: function validateAsParameter(value) {
+      return true;
+    },
+
+    display: function format(value) {
+      return value.toString();
+    }
+  }, options || {});
+
+  if (
+    defaultedOptions.defaultValue !== null &&
+    defaultedOptions.defaultValue !== undefined &&
+    defaultedOptions.validate(defaultedOptions.defaultValue) === false
+  ) {
+    throw new Error('Invalid defaultValue');
+  }
+
+  return {
+    type: defaultedOptions.type,
+
+    defaultValue: defaultedOptions.defaultValue,
+
+    getDisplayName: function display(value) {
+      return defaultedOptions.format(value);
+    },
+
+    validateValue: function validateValue(value) {
+      return defaultedOptions.validate(value);
+    }
+  };
+};
+
+/**
+ * @return {{type, getDisplayName, validateValue, clampValue}}
+ */
+var createNumberParameterShape = function createParameterShape(options) {
+  var defaultedOptions = assign({
+    min: null,
+    max: null,
+    isIntegerOnly: false
+  }, options || {});
+
+  var parameterOptions = assign(pickKeys(defaultedOptions, ['type', 'validate', 'format']), {
+    validate: function validateAsNumber(value) {
+      return (
+        typeof value === 'number' && !isNaN(value) &&
+        (defaultedOptions.min === null || defaultedOptions.min <= value) &&
+        (defaultedOptions.max === null || defaultedOptions.max >= value) &&
+        (!defaultedOptions.isIntegerOnly || isInteger(value))
+      );
+    }
+  });
+
+  var parameter = createParameterShape(parameterOptions);
+
+  return assign({}, parameter, {
+    clampValue: function clampValue(value) {
+      var clampedValue = value;
+
+      if (defaultedOptions.min !== null) {
+        clampedValue = Math.min(defaultedOptions.min, value);
+      }
+
+      if (defaultedOptions.max !== null) {
+        clampedValue = Math.max(defaultedOptions.max, value);
+      }
+
+      return value;
+    }
+  });
+};
+
+var createIntegerParameterShape = function createIntegerParameterShape(options) {
+  return createNumberParameterShape(assign({}, options, {
+    type: PARAMETER_TYPES.INTEGER,
+    isIntegerOnly: true
+  }));
+};
+
+var createRateParameterShape = function createRateParameterShape(options) {
+  return createNumberParameterShape(assign({}, options, {
+    type: PARAMETER_TYPES.RATE,
+    min: 0.0
+  }));
+};
+
+var createChanceParameterShape = function createChanceParameterShape(options) {
+  return createNumberParameterShape(assign({}, options, {
+    type: PARAMETER_TYPES.CHANCE,
+    min: 0.0,
+    max: 1.0
+  }));
+};
+
+var createBooleanParameterShape = function createBooleanParameterShape(options) {
+  return createParameterShape(assign({}, options, {
+    type: PARAMETER_TYPES.BOOLEAN,
+    validate: function validateAsBoolean(value) {
+      return value === true || value === false;
+    }
+  }));
+};
+
+var generatePropertyNames = function generatePropertyNames(parameterName, options) {
+  var defaultedOptions = assign({
+    isHumanizedGetter: true
+  }, options || {});
+
+  var privatePropName = '_' + _s.camelize(parameterName, true);
+  var classifiedPropName = _s.classify(parameterName);
+
+  // e.g. 'maxHp'   -> 'getMaxHp'
+  //      'isEnemy' -> 'isEnemy'
+  var getterName = (
+    defaultedOptions.isHumanizedGetter &&
+    /^(is|has|can|should|will)/.test(parameterName)
+  ) ? _s.camelize(parameterName, true) : 'get' + classifiedPropName;
+
+  return {
+    privatePropName: privatePropName,
+    classifiedPropName: classifiedPropName,
+    getterName: getterName,
+    rawGetterName: 'getRaw' + classifiedPropName,
+    setterName: 'set' + classifiedPropName,
+    validatorName: 'validate' + classifiedPropName,
+    displayerName: 'display' + classifiedPropName
+  };
+};
+
 /**
  * @param {Object} obj
  * @param {string} parameterName
@@ -13,7 +160,7 @@ var utils = require('./lib/utils');
  * @param {(Object|undefined)} options
  */
 var defineParameter = function defineParameter(obj, parameterName, defaultValue, options) {
-  options = assign({
+  var defaultedOptions = assign({
     validate: function(value) {
       return true;
     },
@@ -23,17 +170,9 @@ var defineParameter = function defineParameter(obj, parameterName, defaultValue,
     isHumanizedGetter: true
   }, options || {});
 
-  var privatePropName = '_' + _s.camelize(parameterName, true);
-  var classifiedPropName = _s.classify(parameterName);
-  // e.g. 'maxHp'   -> 'getMaxHp'
-  //      'isEnemy' -> 'isEnemy'
-  var getterName = 'get' + classifiedPropName;
-  if (
-    options.isHumanizedGetter &&
-    /^(is|has|can|should|will)/.test(parameterName)
-  ) {
-    getterName = _s.camelize(parameterName, true);
-  }
+  var propNames = generatePropertyNames(parameterName, {
+    isHumanizedGetter: defaultedOptions.isHumanizedGetter
+  });
 
   //
   // NOTE:
@@ -57,34 +196,29 @@ var defineParameter = function defineParameter(obj, parameterName, defaultValue,
   // ```
   //
 
-  // getRawMaxHp
-  obj['getRaw' + classifiedPropName] = function getRaw() {
-    return this[privatePropName];
+  obj[propNames.rawGetterName] = function getRawParameterValue() {
+    return this[propNames.privatePropName];
   };
 
-  // getMaxHp (or isFooBar or canFooBar etc)
-  obj[getterName] = function get() {
-    return this['getRaw' + classifiedPropName]();
+  obj[propNames.getterName] = function getParameterValue() {
+    return obj[propNames.rawGetterName]();
   };
 
-  // setMaxHp
-  obj['set' + classifiedPropName] = function set(value) {
-    if (!options.validate(value)) {
+  obj[propNames.setterName] = function setParameterValue(value) {
+    if (!defaultedOptions.validate(value)) {
       throw new Error('`' + value + '` is invalid parameter');
     }
-    this[privatePropName] = value;
+    this[propNames.privatePropName] = value;
   };
 
-  // validateMaxHp
-  obj['validate' + classifiedPropName] = options.validate;
+  obj[propNames.validatorName] = defaultedOptions.validate;
 
-  // displayMaxHp
-  obj['display' + classifiedPropName] = function display() {
-    return options.format(this[getterName]());
+  obj[propNames.displayerName] = function displayPropName() {
+    return defaultedOptions.format(obj[propNames.getterName]());
   };
 
   // set default value with validation
-  obj['set' + classifiedPropName](defaultValue);
+  obj[propNames.setterName](defaultValue);
 };
 
 var defineNumberParameter = function defineNumberParameter(obj, parameterName, options) {
@@ -152,11 +286,18 @@ var defineBooleanParameter = function defineBooleanParameter(obj, parameterName,
 
 
 module.exports = {
+  aggregators: aggregators,
+  createBooleanParameterShape: createBooleanParameterShape,
+  createChanceParameterShape: createChanceParameterShape,
+  createIntegerParameterShape: createIntegerParameterShape,
+  createNumberParameterShape: createNumberParameterShape,
+  createRateParameterShape: createRateParameterShape,
+  createParameterShape: createParameterShape,
   defineParameter: defineParameter,
   defineNumberParameter: defineNumberParameter,
   defineIntegerParameter: defineIntegerParameter,
   defineRateParameter: defineRateParameter,
   defineChanceParameter: defineChanceParameter,
   defineBooleanParameter: defineBooleanParameter,
-  aggregators: aggregators
+  generatePropertyNames: generatePropertyNames
 };
